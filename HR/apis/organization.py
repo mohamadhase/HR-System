4bdc3ee37532307c977365d199354f6bde305b61
unknown <you@example.com>
2022-07-20 02:20:17 +0300

    finall cleaning on Organization files

--- HR/apis/organization.py
+++ HR/apis/organization.py
@@ -1,96 +1,76 @@
 # external imports
 from flask_restx import Namespace, Resource
 from flask import abort, request
-
 # internal imports
 from HR import db
 from HR.models.Organization import Organization
 from HR.models.Team import Team
 from HR.models.Authentication import Authentication
 
+# create the namespace for the organization API
 api = Namespace('Organization', description='Organization related API')
 
 
 @api.route('/info')
 class OrganizationInfo(Resource):
-    @api.doc(description='Get inforamtion about the  Organization',security='apikey')
+    @api.doc(description='Get inforamtion about the  Organization', security='apikey')
     @api.response(200, 'success request', Organization.Organization_info)
     @api.response(400, 'invalid arguments')
     @api.response(404, 'Organization not found')
-    # add response for the teams and employees true and false
     @api.param('teams', 'spicify if you want to get teams or not', type=int, default=0)
     @api.param('employees', 'spicify if you want to get employees or not', type=int, default=0)
     @api.response(401, 'Unauthorized')
-    
     @Authentication.token_required
-    def get(orgnization_ID,self):
+    def get(orgnization_ID, self):
+        # get the arguments
         teams = request.args.get('teams')
         employees = request.args.get('employees')
-
-        # validate the orgnization_ID
+        # check if the orgnization exists
         if not Organization.is_exists(orgnization_ID):
             abort(404, 'Organization not found')
-
-        # validate the teams and employees parameters
+        # validate the teams and employees arguments
         try:
-            int(teams)
-        except ValueError as e:
-            abort(400, 'Invalid argument teams -> {teams}')
+            teams = int(teams)
+            if teams not in [0, 1]:
+                raise ValueError
+        except ValueError:
+            abort(400, f'Invalid argument teams -> {teams}')
         try:
-            int(employees)
-
+            employees = int(employees)
+            if employees not in [0, 1]:
+                raise ValueError
         except ValueError as e:
-            abort(400, 'Invalid argument employees -> {employees}')
-
-        if int(teams) not in [0, 1]:
-            abort(400, 'Invalid argument teams -> {teams}')
-        if int(employees) not in [0, 1]:
-            abort(400, 'Invalid argument employees -> {employees}')
-        org_info = Organization.get_info(orgnization_ID,teams,employees)
+            abort(400, f'Invalid argument employees -> {employees}')
+        # get the organization information
+        org_info = Organization.get_info(orgnization_ID, teams, employees)
+        # delete the password from the returned object
         org_info.pop('Password')
-        return org_info,200
-         
+        # return the organization information with success status
+        return org_info, 200
 
-    @api.doc(description='Update the Organization informations',security='apikey')
+    @api.doc(description='Update the Organization informations', security='apikey')
     @api.param('Name', 'New Organization Name')
     @api.param('Address', 'New Adress Description')
     @api.response(200, 'Organization updated', Organization.Organization_info)
     @api.response(404, 'Organization not found')
     @api.response(401, 'Unauthorized')
     @Authentication.token_required
-    def patch(orgnization_ID,self):
-        # validate the orgnization_ID
+    def patch(orgnization_ID, self):
+        # check if the orgnization exists
         if not Organization.is_exists(orgnization_ID):
             abort(404, 'Organization Not Found')
-
         # get current Organization info
         orgnization_info = Organization.get_info(orgnization_ID)
-
         # get the new Organization info
         if request.args.get("Name"):
             orgnization_info['Name'] = request.args.get("Name")
 
         if request.args.get("Address"):
             orgnization_info['Address'] = request.args.get("Address")
-
         # update the Organization info in the database
-        return Organization.update(orgnization_ID, orgnization_info)
-    
-
-
         
-        
-
-@api.route('/teams')
-class OrganizationTeam(Resource):
-    @api.doc(description='Get all teams in the Organization',security='apikey')
-    @api.response(200, 'success request', [Team.team_info])
-    @api.response(404, 'Organization not found')
-    @api.response(401, 'Unauthorized')
-    @Authentication.token_required
-    def get(orgnization_ID,self):
-        # validate the orgnization_ID
-        if not Organization.is_exists(orgnization_ID):
-            abort(404, 'Organization not found')
-        return Organization.get_teams(orgnization_ID)
-    
+        org_info = Organization.update(orgnization_ID, orgnization_info)
+        # delete the password from the returned object
+        org_info.pop('Password')
+        # return the organization information with success status
+        return org_info, 200
--- HR/models/Organization.py
+++ HR/models/Organization.py
@@ -1,58 +1,82 @@
 # external imports
 from flask_restx import fields
-import hashlib
-
-
 # internal imports
 from HR import db
 from HR import api
 
 
 class Organization():
+    # Organization model for the returned data
     Organization_info = api.model('Organization_info',  {
         "Name": fields.String(required=True, description="Organization Name"),
         "Address": fields.String(required=True, description="Organization Address"),
+        "UserName": fields.String(required=True, description="Organization UserName"),
     })
 
-
     @staticmethod
-    
     def get_info(orgnization_ID, teams=False, employees=False):
+        """get the information of an organization from the database 
 
+        Args:
+            orgnization_ID (string): the ID of the organization 
+            teams (bool, optional): flag for the teams to be included in the returned object. Defaults to False.
+            employees (bool, optional): flag for the teams to be included in the returned object. Defaults to False.
+
+        Returns:
+            dict: the organization information 
+        """
         org_ref = db.collection('Organization').document(orgnization_ID)
         orgnization_info = org_ref.get().to_dict()
-        if int(teams) == 1:
+        if teams:
             orgnization_teams_ref = org_ref.collection('Teams').stream()
-            orgnization_teams = []
-            for team in orgnization_teams_ref:
-                orgnization_teams.append(team.to_dict())
+            orgnization_teams = [team.to_dict()
+                                 for team in orgnization_teams_ref]
             orgnization_info['Teams'] = orgnization_teams
-        if int(employees) == 1:
+        if employees:
             orgnization_employees_ref = org_ref.collection(
                 'Employees').stream()
-            orgnization_employees = []
-            for employee in orgnization_employees_ref:
-                orgnization_employees.append(employee.to_dict())
+            orgnization_employees = [employee.to_dict()
+                                     for employee in orgnization_employees_ref]
             orgnization_info['Employees'] = orgnization_employees
         return orgnization_info
 
     @staticmethod
     def update(orgnization_ID, orgnization_info):
+        """update the information of an organization in the database
+
+        Args:
+            orgnization_ID (string): the ID of the organization
+            orgnization_info (dict): dictionary with the new information of the organization
+
+        Returns:
+            dict:the updated organization information
+        """
         org_ref = db.collection('Organization').document(orgnization_ID)
         org_ref.update(orgnization_info)
         return org_ref.get().to_dict()
 
     @staticmethod
     def is_exists(orgnization_ID):
-        org_ref = db.collection('Organization').document(orgnization_ID)        
+        """check if an organization exists in the database
+        Args:
+            orgnization_ID (string): the ID of the organization
+        Returns:
+            bool: True if the organization exists, False otherwise
+        """
+        org_ref = db.collection('Organization').document(orgnization_ID)
         return org_ref.get().exists
 
     @staticmethod
     def get_teams(orgnization_ID):
+        """get the teams of an organization from the database
+
+        Args:
+            orgnization_ID (string): [description]
+
+        Returns:
+            List(dict): List of the teams of the organization
+        """
         teams_ref = db.collection('Organization').document(
             orgnization_ID).collection('Teams').stream()
         teams = [team.to_dict() for team in teams_ref]
         return teams
-
-        
-       
